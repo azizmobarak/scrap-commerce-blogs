14793f824c246125aa4704338e71f0e3468ccc5c
aziz mobarak <mobarakaziz9@gmail.com>
2020-08-11 00:36:31 +0100

    page load speed increase

--- controllers/data.js
+++ controllers/data.js
@@ -6,6 +6,8 @@ const Scrapfunction = (req, res) => {
     try {
         (async() => {
             console.log('1');
+
+            //creating browser and page
             const browser = await puppeteer.launch({
                 headless: false,
                 //args: ['--proxy-server=161.129.155.43:3128']
@@ -14,8 +16,24 @@ const Scrapfunction = (req, res) => {
             console.log('2');
             await browser.userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36")
             const page = await browser.newPage();
-            await page.goto('https://www.shopify.com/blog', { waitUntil: "load", timeout: 50000 });
+            await page.setViewport({ width: 1920, height: 1080 });
+
+
+            //block some features
+            // for javascript block : req.resourceType() === 'script' => not used here
+            await page.setRequestInterception(true);
+            page.on('request', (req) => {
+                if (req.resourceType() === 'image' || req.resourceType() === 'stylesheet' || req.resourceType() === 'font') {
+                    req.abort();
+                } else {
+                    req.continue();
+                }
+            });
+            //page.waitFor(100000);
+            //start open page
+            await page.goto('https://www.shopify.com/blog', { waitFor: 100000 });
             console.log('3')
+
             const result = await page.evaluate(() => {
                 var tab = [];
                 for (var i = 0; i < 50; i++) {
@@ -34,13 +52,13 @@ const Scrapfunction = (req, res) => {
                 return tab;
             });
 
-            await browser.close();
+            /*await browser.close();
 
             var articledata = new data({ data: result });
             articledata.save((err, doc) => {
                 if (err) console.log(err);
                 console.log(doc)
-            });
+            });*/
             res.send(result)
         })();
     } catch (e) {
